85b75250cb1152debb1afdf39ad3eec97b688727
Deputation <58638949+Deputation@users.noreply.github.com>
2024-01-10 20:43:37 +0000

    Typo part two

--- faststr.py
+++ faststr.py
@@ -2,7 +2,7 @@ def string_to_little_endian_hex(s):
     byte_representation = s.encode()
     
     if len(byte_representation) > 64:
-        raise ValueError("The string is too long (> 512 bytes)")
+        raise ValueError("The string is too long (> 64 bytes)")
     
     hex_representation = byte_representation[::-1].hex()
     
